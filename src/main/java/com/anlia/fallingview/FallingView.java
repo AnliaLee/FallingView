e8e31894a7124a91ea1efc8db09ae06533e30f78
AnliaLee <347705530@qq.com>
2017-12-21 09:52:00 +0800

    Initial commit

--- src/main/java/com/anlia/fallingview/FallingView.java
+++ src/main/java/com/anlia/fallingview/FallingView.java
@@ -2,8 +2,6 @@ package com.anlia.fallingview;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
